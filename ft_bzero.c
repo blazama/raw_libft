1094a6891216415ccf2769c78a0939ec423c2e1c
blazama <blancazapicom@gmail.com>
2025-03-01 09:42:27 +0100

    bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,31 +1,18 @@
 /******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bzapico <bzapico@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/25 13:53:18 by bzapico           #+#    #+#             */
-/*   Updated: 2025/03/01 09:42:12 by bzapico          ###   ########.fr       */
+/*   Created: 2025/03/01 09:41:35 by bzapico           #+#    #+#             */
+/*   Updated: 2025/03/01 09:41:43 by bzapico          ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdlib.h>
-# include <unistd.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int ft_isascii(int c);
-int	ft_isprint(int c);
-
-size_t	ft_strlen(const char *s);
-
-void	*ft_memset(void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-
-
-#endif
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
+}
