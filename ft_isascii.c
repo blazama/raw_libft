bfb25638acdf0eda240272cce8f74adc30b517f9
blazama <blancazapicom@gmail.com>
2025-03-01 09:32:22 +0100

    isascii

--- ft_isascii.c
+++ ft_isascii.c
@@ -1,23 +1,18 @@
 /******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bzapico <bzapico@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/25 13:53:18 by bzapico           #+#    #+#             */
-/*   Updated: 2025/03/01 09:32:05 by bzapico          ###   ########.fr       */
+/*   Created: 2025/03/01 09:31:05 by bzapico           #+#    #+#             */
+/*   Updated: 2025/03/01 09:31:19 by bzapico          ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdlib.h>
-# include <unistd.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int ft_isascii(int c);
-#endif
+int	ft_isascii(int c)
+{
+	return (c < 128 && c >= 0);
+}
